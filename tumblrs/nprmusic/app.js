d0c82c4d8a1091f9ec64f5421520ea727cacb825
Tyler Fisher <tyler@tylerjfisher.com>
2016-08-02 11:46:01 -0400

    cooler animation

--- tumblrs/nprmusic/app.js
+++ tumblrs/nprmusic/app.js
@@ -59,4 +59,62 @@ $(function() {
     sizeVideoContainers();
     $video_containers.fitVids({ customSelector: 'video'});
     fixPhotosetWidths();
+
+
+    var colors = new Array(
+        [249,126,80],
+        [249,116,80],
+        [240,91,78],
+        [206,66,66],
+        [183,55,89],
+        [147,50,86],
+        [123,44,98],
+        [101,50,121],
+        [82,65,137],
+        [87,86,165],
+        [65,106,184],
+        [65,91,184]
+    );
+
+    var step = 0;
+    var colorIndices = [0,1,2,3];
+    var gradientSpeed = 0.004;
+
+    function updateGradient() {
+        var c0_0 = colors[colorIndices[0]];
+        var c0_1 = colors[colorIndices[1]];
+        var c1_0 = colors[colorIndices[2]];
+        var c1_1 = colors[colorIndices[3]];
+
+        var istep = 1 - step;
+        var r1 = Math.round(istep * c0_0[0] + step * c0_1[0]);
+        var g1 = Math.round(istep * c0_0[1] + step * c0_1[1]);
+        var b1 = Math.round(istep * c0_0[2] + step * c0_1[2]);
+        var color1 = "rgb("+r1+","+g1+","+b1+")";
+
+        var r2 = Math.round(istep * c1_0[0] + step * c1_1[0]);
+        var g2 = Math.round(istep * c1_0[1] + step * c1_1[1]);
+        var b2 = Math.round(istep * c1_0[2] + step * c1_1[2]);
+        var color2 = "rgb("+r2+","+g2+","+b2+")";
+
+         $('.blog-header').css({
+            background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
+            background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"
+        });
+
+        step += gradientSpeed;
+        if ( step >= 1 ) {
+            step %= 1;
+            colorIndices[0] = colorIndices[1];
+            colorIndices[2] = colorIndices[3];
+
+            //pick two new target color indices
+            //do not pick the same as the current one
+            colorIndices[1] = ( colorIndices[1] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+            colorIndices[3] = ( colorIndices[3] + Math.floor( 1 + Math.random() * (colors.length - 1))) % colors.length;
+
+          }
+    }
+
+    setInterval(updateGradient,10);
 });
